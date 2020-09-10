a7ebbbbf5137c98a7dede76c2b3e71dd6e98f121
andicodetrf <andrea_lau92@hotmail.com>
2020-09-10 17:08:29 +0800

    Updates

---
+++ precache-manifest.7dabfe214b064298d32d554bc17de846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335bf30d1ff2322e9181b6dbf5baebe7",
+    "url": "/randomhood/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/randomhood/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3534959ec05de4eca920",
+    "url": "/randomhood/static/css/main.2f88c190.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/randomhood/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomhood/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3534959ec05de4eca920",
+    "url": "/randomhood/static/js/main.1fb2760a.chunk.js"
+  },
+  {
+    "revision": "ccc0082fd17e0dede276",
+    "url": "/randomhood/static/js/runtime-main.3d9916ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8565c8f00180a78ac3ceac7ad63e65f4.js"
+  "/randomhood/precache-manifest.7dabfe214b064298d32d554bc17de846.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/randomhood/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
